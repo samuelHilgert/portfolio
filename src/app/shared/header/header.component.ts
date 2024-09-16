018629ee740c2514ca764b7b55e5a1a0f776ff12
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-17 00:07:35 +0200

    updated header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';  // Angular Material Modul importieren
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeadlineComponent } from './headline/headline.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule, NavbarComponent],
+  imports: [MatSlideToggleModule, NavbarComponent, HeadlineComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
