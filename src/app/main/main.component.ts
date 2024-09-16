929fd24820270fd29fa3e9808cb62ad11a842a7b
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-17 00:27:16 +0200

    updated components

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
