258482f95aea3433d31986d03f4a74bdd05cc455
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-17 23:01:05 +0200

    updated header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';  // Angular Material Modul importieren
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeadlineComponent } from './headline/headline.component';
+import { SliderComponent } from "./slider/slider.component";
+import { SocialLinksComponent } from "./social-links/social-links.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule, NavbarComponent, HeadlineComponent],
+  imports: [MatSlideToggleModule, NavbarComponent, HeadlineComponent, SliderComponent, SocialLinksComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
