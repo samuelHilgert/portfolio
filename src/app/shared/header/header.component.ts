4e57f24024293b489a54664e0d064a969ed42834
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-18 00:22:34 +0200

    updated topline

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -4,14 +4,25 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeadlineComponent } from './headline/headline.component';
 import { SliderComponent } from "./slider/slider.component";
 import { SocialLinksComponent } from "./social-links/social-links.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule, NavbarComponent, HeadlineComponent, SliderComponent, SocialLinksComponent],
+  imports: [MatSlideToggleModule, NavbarComponent, HeadlineComponent, SliderComponent, SocialLinksComponent, FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isEnglish:boolean = false;  // Standardmäßig auf Englisch (EN)
 
+  toggleLanguage() {
+    this.isEnglish = this.isEnglish;
+    if (!this.isEnglish) {
+      console.log('Sprache auf Englisch umgestellt');
+    } else {
+      console.log('Sprache auf Deutsch umgestellt');
+    }
+  }
 }
+
