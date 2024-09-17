00ec1600026143da1301c5e45fa848c814917fab
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-17 19:33:20 +0200

    updated skill component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, AboutMeComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
