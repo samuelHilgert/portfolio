9fbadce025a0e4f79d9a899fa911fcbbeacda2ea
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-17 21:33:50 +0200

    updated feedback component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FeedbackComponent } from "./feedback/feedback.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, AboutMeComponent, SkillsComponent, ProjectsComponent],
+  imports: [CommonModule, AboutMeComponent, SkillsComponent, ProjectsComponent, FeedbackComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
