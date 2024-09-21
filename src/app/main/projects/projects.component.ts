4abb4c3a3874145889fddf3f4aa235472cbcef95
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-21 20:25:50 +0200

    updated projects

--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -1,13 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgIf],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  arrowState: { [key: number]: boolean } = {};
 
+  constructor() {}
+
+  toggleArrow(index: number, show: boolean) {
+    this.arrowState[index] = show;
+  }
 }
