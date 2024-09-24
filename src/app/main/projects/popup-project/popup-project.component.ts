7588934aef0d611776b01e2814314ac0466ee9e7
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-09-24 17:23:06 +0200

    fixed bug skills popup

--- src/app/main/projects/popup-project/popup-project.component.ts
+++ src/app/main/projects/popup-project/popup-project.component.ts
@@ -2,6 +2,12 @@ import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from
 import { Project } from './../projects.component';
 import { CommonModule, NgIf } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+
+interface SkillImage {
+  skill: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-popup-project',
@@ -13,14 +19,40 @@ import { TranslateModule } from '@ngx-translate/core';
 export class PopupProjectComponent implements OnChanges {
   @Input() openPopup: boolean = false;
   @Input() project!: Project;
-
   @Output() closePopup: EventEmitter<void> = new EventEmitter(); // EventEmitter für Popup-Schließen
+  skillImagesList: SkillImage[] = [];
+  matchingSkillImages: SkillImage[] = []; // Gefilterte Skill-Images
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadSkillImages(); // Lade die Skill-Images bei der Initialisierung
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['project']) {
-      console.log('Ausgewähltes Projekt:', this.project);
+      this.getMatchingSkillImages();
+    }
+  }
+
+  loadSkillImages() {
+    this.http.get<SkillImage[]>('assets/skills.json').subscribe((data: any) => {
+      this.skillImagesList = data.skills;
+      console.log('Skill Images geladen:', this.skillImagesList);
+      // Hier erneut filtern, falls nötig
+      this.getMatchingSkillImages();
+    });
+  }
+
+  getMatchingSkillImages() {
+    if (this.project && this.project.skills) {
+      // Skills des Projekts in ein Array umwandeln
+      const projectSkillsArray = this.project.skills.split(', ').map(skill => skill.trim());
+
+      // Filtern der passenden Images
+      this.matchingSkillImages = this.skillImagesList.filter(skillImage =>
+        projectSkillsArray.includes(skillImage.skill)
+      );
+
+      console.log('Gefilterte Skill-Images:', this.matchingSkillImages);
     }
   }
 
