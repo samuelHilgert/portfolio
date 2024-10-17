66d6a866d774f6d97fa81422d2b832b598ce0b95
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-10-17 19:37:33 +0200

    updated responsive

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,12 +8,14 @@ import { FormsModule } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core'; // Importiere den TranslateService
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../services/translationService';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MatSlideToggleModule,
+    CommonModule,
     NavbarComponent,
     HeadlineComponent,
     SliderComponent,
@@ -24,16 +26,19 @@ import { TranslationService } from '../services/translationService';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-
 export class HeaderComponent {
   isEnglish: boolean = true;
+  openBurgerMenu: boolean = false;
 
-  constructor(private translate: TranslateService, private translationService: TranslationService) {
+  constructor(
+    private translate: TranslateService,
+    private translationService: TranslationService
+  ) {
     // this.translate.setDefaultLang('de');
 
-      // Lade die Sprache aus dem localStorage und setze isEnglish
+    // Lade die Sprache aus dem localStorage und setze isEnglish
     const storedLang = localStorage.getItem('lang') || 'de'; // Fallback auf 'de' wenn kein Wert vorhanden ist
-      this.isEnglish = storedLang === 'de'; // Setze isEnglish basierend auf dem Wert in localStorage
+    this.isEnglish = storedLang === 'de'; // Setze isEnglish basierend auf dem Wert in localStorage
 
     // // Setze die Sprache für die Übersetzungen
     this.translate.setDefaultLang('de'); // Optional: Setze die Standardsprache
@@ -56,5 +61,9 @@ export class HeaderComponent {
       localStorage.setItem('lang', 'de');
     }
     this.translationService.initializeLanguage();
+  }
+
+  openPopupBurgerMenu() {
+   this.openBurgerMenu = true;
   }
 }
