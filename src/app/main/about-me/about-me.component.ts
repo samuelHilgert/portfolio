0dc3a5f8904765c1b757e6ef4d49103a0f98cc7d
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-11-20 15:59:23 +0100

    updated responsive and animation

--- src/app/main/about-me/about-me.component.ts
+++ src/app/main/about-me/about-me.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 
-export class AboutMeComponent {}
+export class AboutMeComponent implements OnInit, OnDestroy {
+/**
+ * This lifecycle hook is called when the component is initialized.
+ * It initializes AOS (Animate On Scroll) with specific animation settings
+ * such as duration, easing, and delay. AOS is responsible for animating
+ * elements as they scroll into view.
+ */
+  ngOnInit(): void {
+    AOS.init({
+      duration: 500,
+      easing: 'ease-in',
+      once: true,
+      offset: 0,
+      delay: 200, 
+    });
+  }
+
+/**
+ * This lifecycle hook is called when the component is destroyed.
+ * It refreshes AOS to ensure that any animations are properly cleaned up.
+ * This is necessary to reset AOS behavior when the component is removed.
+ */
+  ngOnDestroy(): void {
+    AOS.refresh();
+  }
+}
--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 interface Skill {
   skill: string;
@@ -15,11 +17,36 @@ interface Skill {
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit, OnDestroy {
   skills: Skill[] = [];
 
   constructor(private http: HttpClient) {
     this.loadSkills();
+  }
+
+  /**
+   * This lifecycle hook is called when the component is initialized.
+   * It initializes AOS (Animate On Scroll) with specific animation settings
+   * such as duration, easing, and delay. AOS is responsible for animating
+   * elements as they scroll into view.
+   */
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-out',
+      once: true,
+      offset: 0,
+      delay: 200,
+    });
+  }
+
+  /**
+   * This lifecycle hook is called when the component is destroyed.
+   * It refreshes AOS to ensure that any animations are properly cleaned up.
+   * This is necessary to reset AOS behavior when the component is removed.
+   */
+  ngOnDestroy(): void {
+    AOS.refresh();
   }
 
   /**
