89f0c8950bfbec2614079a5eb4ee8ed1ad01ee5c
Samuel Hilgert <samuel.hilgert.webmaster@outlook.de>
2024-12-03 21:32:15 +0100

    fixed bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'index', pathMatch: 'full' },
-    {path:'privacy-policy', component: PrivacyPolicyComponent},
-    {path:'legal-notice', component: LegalNoticeComponent},
+    { path:'privacy-policy', component: PrivacyPolicyComponent },
+    { path:'legal-notice', component: LegalNoticeComponent },
 ];
 
